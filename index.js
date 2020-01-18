13a8c1a0357ae3795e02de28e5946323dc688883
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-18 08:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function (string ="go to the office") {
+    return `This Monday, I will ${string}.`;
+}
+
+function wrapAdjective(adjective="*") {
+//innrerfunction
+    return function (thing ="special") {
+
+        return `You are ${adjective}${thing}${adjective}!`;
+}
+}
+wrapAdjective("||")("a dedicated programmer")
+
